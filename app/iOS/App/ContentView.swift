ef280804b55e092a2145e35b83eb8dab7a4f0788
Julian Visser <12615757+justmejulian@users.noreply.github.com>
2023-10-18 16:00:50 +0200

    add messurements to screen

--- app/iOS/App/ContentView.swift
+++ app/iOS/App/ContentView.swift
@@ -16,8 +16,6 @@ struct ContentView: View {
     
     @State private var isRequestCompleted = true
     
-    @ObservedObject var motionData = MotionData()
-    
     var body: some View {
         VStack(content: {
             Text("VT1 2023")
@@ -31,22 +29,6 @@ struct ContentView: View {
                     .textFieldStyle(RoundedBorderTextFieldStyle())
                     .multilineTextAlignment(.center)
                     .padding(.all)
-                
-                Spacer()
-                Text("Accelerometer Data")
-                    .font(.title)
-                
-                Text("X-Acceleration: \(motionData.Acceleration.x, specifier: "%.2f")")
-                Text("Y-Acceleration: \(motionData.Acceleration.y, specifier: "%.2f")")
-                Text("Z-Acceleration: \(motionData.Acceleration.z, specifier: "%.2f")")
-                
-                Spacer()
-                Text("Gyroscope Data")
-                    .font(.title)
-                
-                Text("X-Gyroscope: \(motionData.Gyroscope.x, specifier: "%.2f")")
-                Text("Y-Gyroscope: \(motionData.Gyroscope.y, specifier: "%.2f")")
-                Text("Z-Gyroscope: \(motionData.Gyroscope.z, specifier: "%.2f")")
                 
                 Spacer()
             })
@@ -72,29 +54,9 @@ struct ContentView: View {
 
     private func addItem() {
         isRequestCompleted = false
-        let data = [
-            "timestamp": Date().timeIntervalSince1970,
-            "device_id": "30D1E9CF-4773-4EA6-8DCE-D9B16ADB47C6",
-            "sensor_id": "8ad3fb4a-6d30-4104-b1b9-f51f970423b1",
-            "x": motionData.Gyroscope.x,
-            "y": motionData.Gyroscope.y,
-            "z": motionData.Gyroscope.z
-        ] as [String : Any]
-        networkViewModel.postDataToAPI(data: data) { error in
-            if let error = error {
-                print("Error: \(error.localizedDescription)")
-            } else {
-                isRequestCompleted = true
-            }
-        }
     }
 
     private func deleteItems(offsets: IndexSet) {
-        withAnimation {
-            for index in offsets {
-                modelContext.delete(items[index])
-            }
-        }
     }
 }
 
--- app/watchOS/App/ContentView.swift
+++ app/watchOS/App/ContentView.swift
@@ -5,14 +5,37 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @ObservedObject var motionData = MotionData()
+    
+    @State var started = false
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        VStack(content: {
+            Text("Accelerometer")
+                .font(.title2)
+            Text("X: \(motionData.Acceleration.x, specifier: "%.2f")")
+            Text("Y: \(motionData.Acceleration.y, specifier: "%.2f")")
+            Text("Z: \(motionData.Acceleration.z, specifier: "%.2f")")
+            
+            Text("Gyroscope")
+                .font(.title2)
+            Text("X: \(motionData.Gyroscope.x, specifier: "%.2f")")
+            Text("Y: \(motionData.Gyroscope.y, specifier: "%.2f")")
+            Text("Z: \(motionData.Gyroscope.z, specifier: "%.2f")")
+            
+            Button(action: handleButton) {
+                Text(started ? "Stop" : "Start")
+                    .font(.title2)
+                    .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/)
+            }
+                .buttonStyle(.borderedProminent) // todo maybe change colors
+                .padding(.all)
+        })
+    }
+    
+    private func handleButton() {
+        started = !started
     }
 }
 
