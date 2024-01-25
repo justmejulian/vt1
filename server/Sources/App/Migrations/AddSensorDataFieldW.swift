b42272dcfd25bc0cf3d2e907aa8b853fd800ff68
Julian Visser <12615757+justmejulian@users.noreply.github.com>
2024-01-25 08:35:00 +0100

    it is generally smart to commit all needed files

---
+++ server/Sources/App/Migrations/AddSensorDataFieldW.swift
@@ -0,0 +1,15 @@
+import Fluent
+
+struct AddSensorDataFieldW: Migration {
+    func prepare(on database: Database) -> EventLoopFuture<Void> {
+        database.schema("sensor_data")
+            .field("w", .double)
+            .update()
+    }
+
+    func revert(on database: Database) -> EventLoopFuture<Void> {
+        database.schema("sensor_data")
+            .deleteField("w")
+            .update()
+    }
+}
