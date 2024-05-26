be14e48b7e8a43df4f374e8cdc335331d5b7cdc3
Julian Visser <12615757+justmejulian@users.noreply.github.com>
2024-05-26 17:38:58 +0200

    added a way to export recordings

--- app/iOS/App/View/RecordingDetailView.swift
+++ app/iOS/App/View/RecordingDetailView.swift
@@ -16,7 +16,10 @@ struct RecordingDetailView: View {
     var sensorData: [SensorData]
 
     @State private var text: String
-
+    @State private var exporting: Bool = false
+    
+    let fileName: String
+    
     init(recording: RecordingData, dataSource: DataSource) {
         self.recording = recording
         self.dataSource = dataSource
@@ -26,10 +29,10 @@ struct RecordingDetailView: View {
         self._sensorData = Query(filter: #Predicate<SensorData> {
             $0.recordingStart == recording.startTimestamp
         })
+        self.fileName = "Recording-\(recording.startTimestamp).csv"
     }
 
     var body: some View {
-        let valluesCount = sensorData.reduce(0) { $0 + $1.values.count }
         VStack{
             Spacer()
             Text("Recording: ")
@@ -45,7 +48,7 @@ struct RecordingDetailView: View {
                 .font(.title3)
             Spacer()
 
-            Text("# of datapoints: " + String(valluesCount))
+            Text("# batches: " + String(sensorData.count))
                 .font(.title2)
 
             Spacer()
@@ -58,6 +61,23 @@ struct RecordingDetailView: View {
                     .frame(maxWidth: .infinity)
             }
                 .buttonStyle(BorderedProminentButtonStyle())
+
+            Button("Export") {
+                exporting = true
+            }
+            .fileExporter(
+                    isPresented: $exporting,
+                    document: generateJson(),
+                    contentType: .json,
+                    defaultFilename: fileName
+                ){ result in
+                    switch result {
+                    case .success(let url):
+                        print("Saved to \(url)")
+                    case .failure(let error):
+                        print(error.localizedDescription)
+                    }
+                }
 
             Button(action: {
                 deleteData()
@@ -81,5 +101,62 @@ struct RecordingDetailView: View {
     func deleteData() {
         Logger.viewCycle.info("deleteData from RecordingDetailView")
         dataSource.removeData(recording)
+    }
+    
+    func generateJson() -> File? {
+        do {
+            let dict: RecordingDict = RecordingDict(
+                exercise: recording.exercise,
+                startTimestamp: recording.startTimestamp.timeIntervalSince1970,
+                // todo we could remove the recordingStart from the sensorData
+                sensorData: sensorData
+            )
+            
+            let json = try JSONEncoder().encode(dict)
+            
+            let file = File(fileName: fileName, file: json as NSData)
+            return file
+        } catch {
+            Logger.viewCycle.error("Failed to generate Json \(error.localizedDescription)")
+            return nil
+        }
+    }
+}
+
+struct RecordingDict: Encodable {
+    var exercise: String
+    var startTimestamp: TimeInterval
+    var sensorData: [SensorData]
+}
+
+import UniformTypeIdentifiers
+
+class File: FileDocument {
+    static var readableContentTypes: [UTType] {
+        [.json]
+    }
+    
+    // Stores the property's value as binary data adjacent to the model storage.
+    @Attribute(.externalStorage)
+    var file: Data
+    
+    var fileName: String
+
+    init(fileName: String, file: NSData) {
+        self.file = file as Data
+        self.fileName = fileName
+    }
+
+    required init(configuration: ReadConfiguration) throws {
+        if let data = configuration.file.regularFileContents {
+            file = data
+        } else {
+            file = Data()
+        }
+        fileName = "NoFileName"
+    }
+    
+    func fileWrapper(configuration: FileDocumentWriteConfiguration) throws -> FileWrapper {
+        return FileWrapper(regularFileWithContents: file as Data)
     }
 }
