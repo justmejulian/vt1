72085fdb71cb5294e18601534d310a3fa5abaed6
Julian Visser <12615757+justmejulian@users.noreply.github.com>
2024-06-02 15:55:23 +0200

    array chunking

---
+++ app/Shared/Util/Array.swift
@@ -0,0 +1,16 @@
+//
+//  Array.swift
+//
+//  Created by Paul Hudson
+//  https://www.hackingwithswift.com/example-code/language/how-to-split-an-array-into-chunks
+
+
+import Foundation
+
+extension Array {
+    func chunked(into size: Int) -> [[Element]] {
+        return stride(from: 0, to: count, by: size).map {
+            Array(self[$0 ..< Swift.min($0 + size, count)])
+        }
+    }
+}
